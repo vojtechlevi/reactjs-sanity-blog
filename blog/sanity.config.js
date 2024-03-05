a74cda3efa535b2826617b54ee347d513b9f6b3c
Levi Ekström <vojtechlevi1@hotmail.com>
2024-03-05 20:17:55 +0100

    added plugin

--- blog/sanity.config.js
+++ blog/sanity.config.js
@@ -1,4 +1,5 @@
 import {defineConfig} from 'sanity'
+import {vercelDeployTool} from 'sanity-plugin-vercel-deploy'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
@@ -10,7 +11,7 @@ export default defineConfig({
   projectId: '7ohjaqen',
   dataset: 'production',
 
-  plugins: [structureTool(), visionTool()],
+  plugins: [structureTool(), visionTool(), vercelDeployTool()],
 
   schema: {
     types: schemaTypes,
